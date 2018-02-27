9c64cb917a2476bf7c35798c949bde3feb250f53
Bryan Williams <brywilldev@gmail.com>
2018-02-27 02:51:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (i === stopValue) {
+      break
+    } else {
+      array[i] = changeValue
+    }
+  }
+}
